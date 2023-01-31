12706078d6551a3f1003339a2c71826c0be8955e
bashunmcgee <bashunmcgee93@gmail.com>
2023-01-31 12:31:47 -0700

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,14 @@
+
+/**
+ * Simple Java Class that reads back Hello
+ */
+public class Hello(){
+	
+	public static void main(String [] args) {
+		//prints Hello to the terminal
+		System.out.println("Hello World");
+	    
+	}
+    
+   
+}
